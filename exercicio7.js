f216047616e39d44ade2adc174c519090595f69b
Jonathas Veras <Jonathasveras42@gmail.com>
2019-03-27 19:59:24 -0300

    Create exercicio7.js

---
+++ exercicio7.js
@@ -0,0 +1,8 @@
+//Feito por Jonathas Veras; Data: 27/3/2019.
+
+var notaA, notaB, notaC, media;
+notaA = parseFloat();
+notaB = parseFloat();
+notaC = parseFloat();
+media = ((notaA*2.0) + (notaB*3.0) + (notaC*5.0)) / 10.0;
+console.log("A media do aluno foi: " + media);
