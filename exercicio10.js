e46a5b45bc94d34ef2a34894be292badcfed7926
Jonathas Veras <Jonathasveras42@gmail.com>
2019-03-27 22:57:13 -0300

    Create exercicio10.js

---
+++ exercicio10.js
@@ -0,0 +1,16 @@
+//Feito por Jonathas Veras; Data: 27/3/2019.
+
+/*
+60----0        (60-60)
+59----1        (59-58)
+58----2        (58-56)
+57----3        (57-54)
+56----4        (56-52)
+
+digito 0 e vai dar 60 logo 60-0
+digito 1 e vai dar 59 logo 60-1
+digito 2 e vai dar 58 logo 60-2
+*/
+var x = parseInt();
+var y = 60-x;
+console.log(y);
