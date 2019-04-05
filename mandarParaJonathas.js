787afdbeada440039bf733bf29a4c00478d8b555
Jonathas Veras <Jonathasveras42@gmail.com>
2019-04-05 16:14:01 -0300

    Add files via upload

---
+++ mandarParaJonathas.js
@@ -0,0 +1,28 @@
+var a=2, b=7, c=3;
+
+
+a>b-1    ||    a>b-2
+falso          falso
+
+resposta: falso
+
+
+c>a      ||   b<c    &&    a+5>b
+verdade      falso         falso
+
+resposta: verdade
+
+a ==c-1  &&   b*2>a*c    &&   (a+b+c)/2<b
+2==2          &&  14>6           &&  6<7
+verdadeiro        verdadeiro        verdadeiro
+
+resposta: verdadeiro
+
+c-a>b/3 || !(5+c-a<=12)
+3-2>7/3 || !(6<=12)
+resposta: falso
+
+c*3>= b && 5+a<=b&& b-c>=a*2&&b*3>(b+c)*2
+9>=7   && 7<=7  &&  4>=4 && 9>10*2
+verdadeiro      verdadeiro      verdadeiro    verdadeiro
+resposta: vedadeiro
